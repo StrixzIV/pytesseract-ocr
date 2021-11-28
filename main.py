ef2ab6bc6674a135a19e1f835aab90e4cbdf8a3a
Jirayu Kaewsing <46487993+StrixzIV@users.noreply.github.com>
2021-11-28 13:38:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pytesseract as tess
 from PIL import Image
 
-tess.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+tess.pytesseract.tesseract_cmd = r'PATH\TO\tesseract.exe'
 
 image = Image.open('testocr.png')
 text = tess.image_to_string(image, lang = 'eng')
